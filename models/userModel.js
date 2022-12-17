39f3b3df4e3ed1617a8e78b726856d978f39fe28
elman44 <lukasz.badocha@gmail.com>
2022-12-17 22:20:32 +0100

    user hash password

--- models/userModel.js
+++ models/userModel.js
@@ -20,12 +20,16 @@ User.pre("save", function (next) {
   }
 
   bcrypt.genSalt(10, function (err, salt) {
-    if (err) return next(err);
+    if (err) {
+      return next(err);
+    }
 
-    bcrypt.hash(user.password, salt, function (err, hash) {
-      if (err) return next(err);
+    bcrypt.hash(user.password, salt, function (err, hashPassword) {
+      if (err) {
+        return next(err);
+      }
 
-      user.password = hash;
+      user.password = hashPassword;
       next();
     });
   });
